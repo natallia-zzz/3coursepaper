ac23fbb5d3efa41603093a5e143e4bdafd391cad
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2022-05-14 19:10:09 +0300

    fixed function

--- YeonFitting.py
+++ YeonFitting.py
@@ -11,7 +11,7 @@ def extract_file_data(path):
 
 
 def func(x, mu, beta):
-	return np.exp(0.053*x[0] + x[1]*mu*(1 - np.exp(-beta * x[0])-beta*x[0])/(beta**2))
+	return np.exp(-0.053*x[0] + x[1]*mu*(1 - np.exp(-beta * x[0])-beta*x[0])/(beta**2))
 
 
 x, t = np.linspace(0, 6, 13), np.linspace(0,15,4)
@@ -28,16 +28,32 @@ dataA15 = extract_file_data('yeon-data/A15.json')
 
 data = dataA0['Cexp']+ dataA5['Cexp'] + dataA10['Cexp']+ dataA15['Cexp']
 ydata = [i/100 for i in data]
-#popt, pcov = curve_fit(func, x_data, ydata)
-#popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185])
-#popt, pcov = curve_fit(func, x_data, ydata, p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]))
-#popt, pcov = curve_fit(func, x_data, ydata, bounds = ([0.01,0.1],[0.1,0.7]),method = 'trf',loss = 'huber')
-# popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]), method = 'trf', tr_solver = 'lsmr',)
-popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]),method = 'trf', loss = 'huber', ftol = 1e-10, tr_solver = 'lsmr',)
-
-
+popt, pcov = curve_fit(func, x_data, ydata)
 print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
-
 y_est = func(x_data, *popt)
+print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
 
+popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185])
+print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
+y_est = func(x_data, *popt)
+print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
+
+popt, pcov = curve_fit(func, x_data, ydata, p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]))
+print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
+y_est = func(x_data, *popt)
+print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
+
+popt, pcov = curve_fit(func, x_data, ydata, bounds = ([0.01,0.1],[0.1,0.7]),method = 'trf',loss = 'huber')
+print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
+y_est = func(x_data, *popt)
+print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
+
+popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]), method = 'trf', tr_solver = 'lsmr',)
+print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
+y_est = func(x_data, *popt)
+print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
+
+popt, pcov = curve_fit(func, x_data, ydata,p0 = [0.0469,0.185], bounds = ([0.01,0.1],[0.1,0.7]),method = 'trf', loss = 'huber', ftol = 1e-10, tr_solver = 'lsmr',)
+print('fit: mu=%5.3f, beta=%5.3f' % tuple(popt))
+y_est = func(x_data, *popt)
 print('error: '+str(np.sum(np.abs(np.array(ydata)-np.array(y_est)))))
